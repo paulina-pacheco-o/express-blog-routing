0591b2f924ed13ce103e779152044269301c9773
paulina-pacheco-o <paulimichi05@gmail.com>
2025-08-01 14:24:51 +0200

    show router

--- routers/posts.js
+++ routers/posts.js
@@ -2,7 +2,20 @@ const express = require('express');
 
 const router = express.Router();
 
-//index
+//Index
 router.get('/', (req, res) => {
   res.send('Lista post')
-})
+});
+
+//Show
+router.get('/:id', (req, res) => {
+  res.send(`Dettaglio del post con id ${req.params.id}`);
+});
+
+//Create
+
+
+//Update
+
+
+//Delete
