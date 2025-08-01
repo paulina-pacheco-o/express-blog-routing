7bfc5c639f4605370a02fde1a82fa5b8ea1ebc54
paulina-pacheco-o <paulimichi05@gmail.com>
2025-08-01 13:34:35 +0200

    first commit

---
+++ app.js
@@ -0,0 +1,47 @@
+const express = require('express')
+
+const app = express();
+
+app.use(express.static('public'))
+
+const port = 3000
+
+app.get('/', (req, res) => {
+  console.log('Server del mio blog')
+})
+
+app.listen(port, () => {
+  console.log('Server del mio blog')
+})
+
+app.get('/bacheca', (req, res) => {
+  const post = [
+    {
+      titolo: "Italia",
+      contenuto: "Lorem ipsum dolor",
+      img: "/assets/italia.jpg",
+      tags: ["italia", "lorem"],
+    }, {
+      titolo: "Spagna",
+      contenuto: "Lorem ipsum dolor",
+      img: "/assets/spagna.jpg",
+      tags: ["spagna", "lorem"],
+    }, {
+      titolo: "Grecia",
+      contenuto: "Lorem ipsum dolor",
+      img: "/assets/grecia.jpg",
+      tags: ["grecia", "lorem"],
+    }, {
+      titolo: "Cuba",
+      contenuto: "Lorem ipsum dolor",
+      img: "/assets/cuba.jpg",
+      tags: ["cuba", "lorem"],
+    }, {
+      titolo: "Thailandia",
+      contenuto: "Lorem ipsum dolor",
+      img: "/assets/thailandia.jpg",
+      tags: ["thailandia", "lorem"],
+    }
+  ];
+  res.json(post)
+})
